ab416d110f90b457e660e433c253e1f32f4ea0ea
hayato-coder <hayatoto0702@gmail.com>
2021-09-29 21:01:04 +0900

    station3

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,5 +1,26 @@
 class Admin::MoviesController < ApplicationController
+
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      
+    else
+      flash[:danger] = "登録に失敗しました"
+      render 'new'
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:name, :year, :description,
+                                   :image_url, :is_showing)
+    end
 end
