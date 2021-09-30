0e07339e616460726d7a1d17a816cc9d75fd73f9
hayato-coder <hayatoto0702@gmail.com>
2021-09-30 10:27:01 +0900

    station4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, length: {maximum: 160}, uniqueness: true
+  validates :year, length: {maximum: 45}
+  validates :image_url, length: {maximum: 150}
   validates :is_showing, presence: true
 end
