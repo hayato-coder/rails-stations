957d870a6ebb55cbb104a99b3fd297d5e9fd6bed
hayato-coder <hayatoto0702@gmail.com>
2021-09-30 10:31:17 +0900

    station4

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -28,7 +28,7 @@ class Admin::MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    if @movie.update_attributes(movie_params)
+    if @movie.updates(movie_params)
       redirect_to action: "show"
     else
       flash[:danger] = "登録に失敗しました"
